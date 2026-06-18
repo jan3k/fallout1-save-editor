70799e9501eb41a9e6d5f196a81119955947f834
Jan Augustyniak <37340293+jan3k@users.noreply.github.com>
2026-06-18 17:48:29 +0200

    Add Fallout 2 format package

---
+++ src/f1se/format/fallout2/__init__.py
@@ -0,0 +1,6 @@
+"""Fallout 2 read-only format support."""
+from __future__ import annotations
+
+from f1se.format.fallout2.save_dat import Fallout2SaveDat, probe_fallout2
+
+__all__ = ["Fallout2SaveDat", "probe_fallout2"]
